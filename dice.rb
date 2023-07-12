5e677658d66e12d81ec6e04b80c1a73617623c1e
siddiquina <137005373+siddiquina@users.noreply.github.com>
2023-07-12 02:00:36 +0000

    2d6

--- dice.rb
+++ dice.rb
@@ -1,8 +1,14 @@
 # /dice.rb
 
 require "sinatra"
-
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "Hello World"
@@ -14,4 +20,15 @@ end
 
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
