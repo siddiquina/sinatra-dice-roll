83a1db3f00e197adc091703d6349c96841f8ed02
siddiquina <137005373+siddiquina@users.noreply.github.com>
2023-07-12 02:35:54 +0000

    done

--- dice.rb
+++ dice.rb
@@ -11,7 +11,15 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
@@ -30,5 +38,39 @@ get("/dice/2/6") do
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
   "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+	second_die = rand(1..10)
+  sum = first_die + second_die
+
+	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+	"<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  die = rand(1..20)
+
+	outcome = "You rolled a #{die}."
+
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+	second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die
+
+	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+
+	"<h1>5d4</h1>
    <p>#{outcome}</p>"
 end
